8193fdae341025c1589e143be925b66ee49d7be0
guilherm.henrique <guilhermegilipe.henriquesamaral@students.hevs.ch>
2023-06-16 15:47:02 +0200

    add batiment to admin and map

--- django/geoweb/swissgeo/admin.py
+++ django/geoweb/swissgeo/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.contrib.gis import admin
 
-from .models import Canton, Piste, Remontee
+from .models import Canton, Piste, Remontee, Batiment
 
 # Register your models here.
 admin.site.register(Remontee)
 admin.site.register(Piste)
+admin.site.register(Batiment)
 admin.site.register(Canton, admin.OSMGeoAdmin)
