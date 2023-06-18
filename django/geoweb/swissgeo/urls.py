1afe3ade3b7dbf7904e5b47df6cadea47655151a
guilherm.henrique <guilhermegilipe.henriquesamaral@students.hevs.ch>
2023-06-18 20:43:12 +0200

    Display distance in distance.html

--- django/geoweb/swissgeo/urls.py
+++ django/geoweb/swissgeo/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
@@ -21,4 +21,6 @@ urlpatterns = [
          views.getLengthRemontee, name='get_length_remontee'),
     path('get_area_batiment/<str:name>/',
          views.getAreaBatiment, name='get_area_batiment'),
+    re_path(r'^closestPiste/(?P<lat>[-+]?[0-9]*\.?[0-9]+)/(?P<lng>[-+]?[0-9]*\.?[0-9]+)/$',
+            views.closestPiste, name='closest_piste'),
 ]
